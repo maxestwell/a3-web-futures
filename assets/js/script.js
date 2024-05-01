7886f570ee0b918138c665efde1d6eede8a979b8
maxestwell <maxwellpcorcoran@gmail.com>
2024-05-01 15:01:21 +1000

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// This is a JavaScript code block
+// You can start writing your code here
+
+// Example: Print "Hello, World!" to the console
+console.log("Hello, World!");
+
+// Example: Declare a variable and assign a value
+let message = "Welcome to JavaScript!";
+console.log(message);
+
+// Example: Create a function
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Example: Call the function
+greet("Max");
+
+// Feel free to modify and add more code as needed
