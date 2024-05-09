029f9a3b18e5f7ce5fb2aeda4e93e6739adc2d2f
maxestwell <maxwellpcorcoran@gmail.com>
2024-05-09 15:33:40 +1000

    first

--- assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-const playBTN = document.getElementById("play-btn");
-const synth = new Tone.Synth().toDestination();
-
-playBTN.addEventListener("click", () => {
-  if (Tone.context.state !== "running") {
-    Tone.start();
-  }
-  synth.triggerAttackRelease("C3", "8n");
-});
